2590c33bd2da17a06fb4d67b796c358aa77f356b
Oneill38 <megan.oneill38@gmail.com>
2014-04-28 15:47:21 -0400

    fb sign in works
    added name column

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
 def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
@@ -19,7 +19,6 @@ def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
                             email:auth.info.email,
                             password:Devise.friendly_token[0,20],
                           )
-
       end
     end
   end
