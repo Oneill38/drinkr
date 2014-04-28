1cf34535f81e15f5e0da54986c160f5b778f702b
talalc <talalchoudry@gmail.com>
2014-04-28 16:07:23 -0400

    merchant styling

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,7 +1,8 @@
 var Landing = {
 
   onReady: function() {
-    $("<input>").attr( {type:'text', id: 'search_drinks', placeholder: 'enter location'}).css( {position:'absolute', top: '50%', right: '50%' }).appendTo("body");
+    $("<div>").addClass("merchants").css( { position:'absolute', top: '20%' }).appendTo("body")
+    $("<input>").attr( {type:'text', id: 'search_drinks', placeholder: 'enter location'}).css( {position:'absolute', top: '10%', right: '50%' }).appendTo("body");
     $("#search_drinks").on('keyup', function(event){
       if (event.which === 13 || event.keyCode === 13){
         var searchString = $(this).val();
@@ -20,7 +21,7 @@ var Landing = {
             dataType: "json",
             data: { latitude: latitude, longitude: longitude }
           }).done(function(data){
-            debugger;
+            console.log(data);
             data.merchants.forEach(function(merchant){
               addMerchant(merchant);
             });
@@ -42,8 +43,9 @@ $(document).ready(Landing.onReady);
     $("<h1>").text(merchant.summary.name).appendTo(header);
     var section = $("<section>").appendTo(merchantArticle);
     $("<p>").text(merchant.summary.phone).appendTo(section);
-    $("<p>").text(merchant.summary.description).appendTo(section);
-    $("<p>").text(merchant.summary.overall_rating).appendTo(section);
-    $("<p>").text(merchant.location.distance).appendTo(section);
-    merchantArticle.appendTo("body");
+    $("<p>").text('Rating: ' + merchant.summary.overall_rating + " based on " + merchant.summary.num_ratings + " ratings" ).appendTo(section);
+    $("<p>").text('Distance: ' + merchant.location.distance).appendTo(section);
+    $("<p>").text(merchant.location.street).appendTo(section);
+    // $("<p>").text(merchant.summary.description).appendTo(section);
+    merchantArticle.appendTo(".merchants");
   }
