19dbfc54f621c96eaa075852408e595856ff53fc
Oneill38 <megan.oneill38@gmail.com>
2014-04-29 17:39:54 -0400

    working on venmo sign in

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,2 +1,19 @@
 require 'spec_helper'
 
+describe "login" do
+  it "should let a user create an account" do
+    visit '/users/sign_up'
+    click_on 'Sign up'
+    fill_in 'Email', with: "me@me.com"
+    fill_in 'Password', with: "lalalala"
+    fill_in 'Password confirmation', with: "lalalala"
+    click_button 'Sign up'
+    expect(page).to have_content('Hi')
+  end
+
+
+
+
+end
+
+
