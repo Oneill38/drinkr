2fb8ec39c9906acb9fdd62050e83e6cc325a9114
rew38 <raphaelwertheimer@gmail.com>
2014-04-29 18:12:30 -0400

    sign up test complete

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -7,9 +7,10 @@ describe "signing up process" do
     fill_in 'Email', with: 'raph@raph.com'
     fill_in 'Password', with: 'raphraph'
     fill_in 'Password confirmation', with: 'raphraph'
+    click_button 'Sign up'
     #Given a user who is trying to sign up
     #When he inputs his email and password
     #then he should receive a "thank you for signing up" message
-    expect
+    expect(page).to have_content('Hi,')
   end
 end
