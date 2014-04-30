f9b0eecf9415738d5f98f81612382937365e2c56
rew38 <raphaelwertheimer@gmail.com>
2014-04-30 18:31:49 -0400

    got guest token test to pass, about to play with WaitForAjax helper

--- spec/features/landing_spec.rb
+++ spec/features/landing_spec.rb
@@ -19,6 +19,21 @@ describe "visiting the site" do
       expect(page).to have_content "Absolut"
     end
 
+    it "gets a guest token", :js => true do
+      visit root_path
+      fill_in 'search_merchants', with: 'soho'
+      find_field('search_merchants').native.send_key("\n")
+      find('#merchant-30782').click
+      find('#item-N292.item section button').click
+      # click_button 'Add to Cart'
+      expect(page).to have_selector('#item_count', text: '0 item(s)')
+      expect(page).to have_selector('#item_count', text: '1 item(s)')
+      # expect(page).not_to have_selector('#item_count', text: '0 item(s)')
+
+
+
+    end
+
   end
 
 end
