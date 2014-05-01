4c439911e512f5d0e074c07524cc60f9ddc795e1
cohart <me@colin-hart.com>
2014-05-01 14:05:21 -0400

    removed notes from venmo.js

--- app/assets/javascripts/venmo.js
+++ app/assets/javascripts/venmo.js
@@ -1,21 +1,4 @@
-// #sandbox
-// response = HTTParty.post('https://sandbox-api.venmo.com/v1/payments',
-//   :query => { :access_token => 'Px2vW5wqcsSKPuEN6XVbDAw2aXwWBLZT',
-//               :user_id => 145434160922624933,
-//               :email => 'venmo@venmo.com',
-//               :note => 'it is complete',
-//               :amount => -0.10})
 
-// #getting my profile
-// response = HTTParty.get('https://api.venmo.com/v1/me?', :query => {:access_token => 'Px2vW5wqcsSKPuEN6XVbDAw2aXwWBLZT'})
-
-// #getting my friends
-// response = HTTParty.get('https://api.venmo.com/v1/users/1409404993798144722/friends?',
-//   :query => {:access_token => 'Px2vW5wqcsSKPuEN6XVbDAw2aXwWBLZT'})
-
-// #getting jenna's friends
-// response = HTTParty.get('https://api.venmo.com/v1/users/1103723682070528280/friends?',
-//   :query => {:access_token => 'Px2vW5wqcsSKPuEN6XVbDAw2aXwWBLZT'})
 
 var venmoFunctioning = {
 
